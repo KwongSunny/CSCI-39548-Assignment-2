1a2cea74728f92e6267d774f4f0a141f824afba7
Sunny Kwong <sunnykwong91500@gmail.com>
2022-03-09 19:59:36 -0500

    fixing map

--- myMap.js
+++ myMap.js
@@ -1,6 +1,6 @@
 // MAP //
-Array.prototype.myMap = function(callbackFn, thisArg) {
-    let resultArr = thisArg?[...thisArg]:[...this];
+Array.prototype.myMap = function(callbackFn) {
+    let resultArr = [...this];
 
     for(let i = 0; i < resultArr.length; i++){
         resultArr[i] = callbackFn(resultArr[i]);
@@ -14,13 +14,6 @@ let myArray = [1,2,3,4,5];
 console.log("myMap");
 console.log(myArray.myMap((x) => {return x * 2}));
 console.log(myArray);
-
-myArray = [1,2,3,4,5];
-myArray2 = [3,4,5,6,7];
-console.log("myMap(2)")
-console.log(myArray.myMap((x) => {return x * 2}, myArray2));
-console.log(myArray);
-
 
 myArray = [1,2,3,4,5];
 console.log("map")
